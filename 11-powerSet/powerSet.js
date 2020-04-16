c3415faa8e24a5f682a0b781e6d2c1f280253d16
lmt <1571min@naver.com>
2020-04-16 09:07:23 +0900

    update

--- 11-powerSet/powerSet.js
+++ 11-powerSet/powerSet.js
@@ -22,32 +22,46 @@
 const powerSet = function (str) {
   // TODO: Your code here!
   //조합
-  let strArr = ['', ...str.split('')];
+
+  let unique = new Set();
+  for (let i = 0; i < str.length; i++) {
+    unique.add(str[i]);
+  }
+  let uniqStr = Array.from(unique);
   let indexArr = [];
   let result = [];
-  let checkIndex = Array(strArr.length).fill(0);
-  let recursion = () => {
-    //탈출조건
-    //index ===length
-    if (indexArr.length === strArr.length - 1) {
-      //  결과배열에 배열 문자열 모두 합해서 push
-      let strTemp = indexArr.join('');
-      if (!result.includes(strTemp)) {
-        result.push(strTemp);
-      }
-      return;
-    }
-    for (let i = 0; i < strArr.length; i++) {
+  let checkIndex = Array(uniqStr.length).fill(0);
+
+  let recursion = (idx) => {
+    result.push(indexArr.join(''));
+    for (let i = idx; i < uniqStr.length; i++) {
       //재귀 호출
-      if (checkIndex[i] === 1 && i !== 0) continue;
+      if (checkIndex[i] === 1) continue;
       checkIndex[i] = 1;
-      indexArr.push(strArr[i]);
-      recursion();
+      indexArr.push(uniqStr[i]);
+      recursion(i + 1);
       indexArr.pop();
       checkIndex[i] = 0;
     }
   };
-  recursion();
+  recursion(0);
 
+  // let result = [];
+  // let unique = new Set();
+  // // unique.add('')
+  // for (let i = 0; i < str.length; i++) {
+  //   unique.add(str[i]);
+  // }
+  // //set->array 변경
+  // let uniqStr = Array.from(unique);
+  // function recursion(string, start) {
+  //   result.push(string);
+  //   for (let i = start; i < uniqStr.length; i++) {
+  //     const uniqStrElement = uniqStr[i];
+  //     //안녕 힘쎄고 좋은아침 난 건너뛴다 인덱스를
+  //     recursion(string + uniqStrElement, i + 1);
+  //   }
+  // }
+  // recursion('', 0);
   return result.sort();
 };
