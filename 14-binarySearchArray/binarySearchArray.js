6be13df5db1711c7828563be927f59ade45969dd
lmt <1571min@naver.com>
2020-04-20 09:20:59 +0900

    completed toy14

--- 14-binarySearchArray/binarySearchArray.js
+++ 14-binarySearchArray/binarySearchArray.js
@@ -5,10 +5,30 @@
  *
  * let index = binarySearch([1, 2, 3, 4, 5], 4);
  * console.log(index); // [3]
- * 
+ *
  * 참고 : https://qph.fs.quoracdn.net/main-qimg-742d049387316193be2d097fe7a499de
  */
 
-const binarySearch = function(array, target) {
+const binarySearch = function (array, target) {
   // TODO: Your code here!
+  //이진 탐색 트리
+  // 초기 값
+  // 마지막 값
+  // 마지막 값이 초기값보다 커질 때 까지
+  // 중간 값보다 크면 초기값 변경
+  //중간 값보다 작으면 마지막 값 변경
+  // 같으면 리턴
+  let first = 0;
+  let last = array.length - 1;
+  while (last >= first) {
+    let middle = Math.floor((first + last) / 2);
+    if (array[middle] === target) {
+      return middle;
+    } else if (array[middle] < target) {
+      first = middle + 1;
+    } else {
+      last = middle - 1;
+    }
+  }
+  return null;
 };
