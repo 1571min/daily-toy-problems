8fb03eacd5a70dce2a9f0589675d3472bc512ddb
lmt <1571min@naver.com>
2020-04-21 09:52:13 +0900

    completed toy15

--- 15-rangeClass/rangeClass.js
+++ 15-rangeClass/rangeClass.js
@@ -38,18 +38,41 @@
  * evenNumbers.includes(2) should be true, evenNumbers.includes(3) should be false
  */
 
-
-var Range = function(start, end, step) {
+var Range = function (start, end, step) {
+  this.start = start;
+  this.end = end !== undefined ? end : start;
+  this.step = step !== undefined ? step : 1;
 };
 
 Range.prototype.size = function () {
+  return Math.floor((this.end - this.start) / this.step) + 1;
 };
 
 Range.prototype.each = function (callback) {
+  if (this.step < 0) {
+    for (let i = this.start; i >= this.end; i += this.step) {
+      callback(i);
+    }
+  } else {
+    if (this.start > this.end) {
+      for (let i = this.start; i >= this.end; i -= this.step) {
+        callback(i);
+      }
+    } else {
+      for (let i = this.start; i <= this.end; i += this.step) {
+        callback(i);
+      }
+    }
+  }
 };
 
 Range.prototype.includes = function (val) {
+  for (let i = this.start; i <= this.end; i += this.step) {
+    if (val === i) {
+      return true;
+    }
+  }
+  return false;
 };
 
 var range = new Range(1);
-
