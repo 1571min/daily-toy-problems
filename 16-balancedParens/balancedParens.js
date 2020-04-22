fdcc55195cb7a816f4f0f4ed26c4c64beb3d4ac8
lmt <1571min@naver.com>
2020-04-22 09:57:13 +0900

    completed toy16

--- 16-balancedParens/balancedParens.js
+++ 16-balancedParens/balancedParens.js
@@ -23,7 +23,69 @@
  *
  *
  */
-var balancedParens = function(input){
+
+function checkParens(open, close) {
+  if (open === '(' && close === ')') {
+    return true;
+  } else if (open === '{' && close === '}') {
+    return true;
+  } else if (open === '[' && close === ']') {
+    return true;
+  }
+  return false;
+}
+function isParens(ele) {
+  return (
+    ele === '(' ||
+    ele === '[' ||
+    ele === '{' ||
+    ele === ')' ||
+    ele === '}' ||
+    ele === ']'
+  );
+}
+
+var balancedParens = function (input) {
+  let inputArr = input.split('');
+  inputArr = inputArr.filter((ele) => {
+    return isParens(ele);
+  });
+  let bucket = [];
+
+  for (let i = 0; i < inputArr.length; i++) {
+    if (inputArr[i] === '(' || inputArr[i] === '{' || inputArr[i] === '[') {
+      //여는 괄호 일 때
+      bucket.push(inputArr[i]);
+    } else {
+      // 닫는 괄호 일 때
+      let check = false;
+      let temp = [];
+      while (bucket.length !== 0) {
+        let paren = bucket.pop();
+        if (checkParens(paren, inputArr[i])) {
+          //짝이 맞으면 남은 temp모두 bucket으로
+          while (temp.length !== 0) {
+            bucket.push(temp.pop());
+          }
+          check = true;
+          break; //while 탈출
+        } else {
+          temp.push(bucket.pop());
+        }
+      }
+      if (!check) {
+        if (temp.length === 0) {
+          bucket.push(inputArr[i]);
+        }
+        while (temp.length !== 0) {
+          bucket.push(temp.pop());
+        }
+      }
+    }
+  }
+  if (bucket.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
-
-
