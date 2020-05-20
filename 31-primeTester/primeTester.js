d40aa502b8b13cee577dd5f63c4e7aa045d94434
lmt <1571min@naver.com>
2020-05-20 09:45:23 +0900

    update toy31

--- 31-primeTester/primeTester.js
+++ 31-primeTester/primeTester.js
@@ -4,12 +4,27 @@
  * a prime number, false if it's not.
  */
 
-var primeTester = function(n) {
-  if(typeof n !== 'number' || n < 1 || n % 1 !== 0){
+var primeTester = function (n) {
+  if (typeof n !== 'number' || n < 1 || n % 1 !== 0) {
     // n isn't a number or n is less than 1 or n is not an integer
     return false;
   }
+  let checkarray = [];
+  for (let i = 2; i < n; i++) {
+    checkarray[i] = i;
+  }
   // TODO: return true if n is prime, false otherwise
+  if (n === 1) return false;
+  for (let i = 2; i < n; i++) {
+    if (checkarray[i] === 0) continue;
+    for (let j = 2; j < n; j += i) {
+      checkarray[j] = 0;
+    }
+    if (n / i === 0) {
+      return false;
+    }
+  }
+  return true;
 };
 
 /* Extra credit: Write a function that generates a list of all prime numbers
@@ -18,7 +33,4 @@ var primeTester = function(n) {
  * saucy, check out the Sieve of Atkin.)
  */
 
-var primeSieve = function (start, end) {
-};
-
-
+var primeSieve = function (start, end) {};
