f3301adc4745cbaab2441669a83a112bd7d8158d
lmt <1571min@naver.com>
2020-06-16 09:15:37 +0900

    not yet

--- 49-spiralTraversal/spiralTraversal.js
+++ 49-spiralTraversal/spiralTraversal.js
@@ -14,7 +14,30 @@
     returns [1, 2, 3, 6, 9, 8, 7, 4, 5]
  */
 
-var spiralTraversal = function(matrix){
-
+var spiralTraversal = function (matrix) {
   // TODO: Implement me!
+  let temp = 0;
+  if (matrix[0].length >= 2) {
+    temp = 3 + 2 * (matrix[0].length - 2);
+    if (matrix.length > matrix[0].length) temp += 1;
+  } else {
+    temp = 1;
+  }
+  let result = [];
+  let row = matrix[0].length;
+  let column = matrix.length;
+  let
+  for (let i = 0; i < temp; i++) {
+    if (i % 2 === 0) {
+      // 가로로 갈 때
+      for (let j = 0; j < row - i / 2; j++) {
+        temp['고정'][]
+      }
+    } else {
+      // 세로로 갈 때
+      for (let j = 0; j < column + 1 - i; j++) {
+        temp[]['고정']
+      }
+    }
+  }
 };
